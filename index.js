b31eb9f8e4b0536ef1bbc5b6d5051afaba732ddb
dschreckling <dschreckling@users.noreply.github.com>
2017-02-13 22:55:10 +0100

    Scan for idm-token
    This prototype enhancement scans for the node during deployment and replaces its credentials with the correct credentials for client authentication with IDM

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var http = require('http');
+var path = require('path');
+var bodyParser = require('body-parser');
 var express = require("express");
 var RED = require("node-red");
+
 var conf = require("./conf/node-red-security-conf");
 
 // Create an Express app
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+    .use(bodyParser.json());
 
 // Add a simple route for static content served from 'public'
 app.use("/",express.static("public"));
@@ -18,8 +24,32 @@ var settings = conf.nodered;
 // Initialise the runtime with a server and settings
 RED.init(server,settings);
 
+var deployURL = settings.httpAdminRoot.match(path.sep + "$") ? settings.httpAdminRoot : settings.httpAdminRoot + path.sep;
+deployURL += "flows";
+
+app.post(deployURL, function(req, res, next) {
+    if(req.body && req.body.flows) {
+        var flows = req.body.flows;
+        for(var n in flows) {
+            var node = flows[n];
+            if(conf.security && node.type === conf.security.idmNodeType) {
+                // deployment is differential, so always create
+                // the object, if it does not exist
+                if(!node.credentials)
+                    req.body.flows[n].credentials = {};
+
+                // set clientId and clientSecret in the flow
+                req.body.flows[n].credentials.clientId = conf.security.clientId;
+                req.body.flows[n].credentials.clientSecret = conf.security.clientSecret;
+            }
+        }
+    }
+    next();
+});
+
 // Serve the editor UI from /red
 app.use(settings.httpAdminRoot,RED.httpAdmin);
+
 
 // Serve the http nodes UI from /api
 app.use(settings.httpNodeRoot,RED.httpNode);
