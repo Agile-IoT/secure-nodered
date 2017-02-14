a1e1d9f95ca20142d9a466656d134ccef206ea38
david <nopbyte>
2017-02-14 14:55:50 +0100

    include token validation and node-red autologin

--- conf/node-red-security-conf.js
+++ conf/node-red-security-conf.js
@@ -1,15 +1,31 @@
 module.exports = {
-   "security":{
-      clientId : "clientId",
-      clientSecret : "secret",
-      userid : "bob!@!agile-local",
-      idmNodeType: "idm-token",
-      port : 8000
+  "security": {
+    clientId: "clientId",
+    clientSecret: "secret",
+    userid: "bob!@!agile-local",
+    idmNodeType: "idm-token",
+    port: 8000
+  },
+  "idm": {
+    userInfoUrl: "http://localhost:3000/oauth2/api/userinfo"
+  },
+  "nodered_integration":{//this is used to login over the browser with node-red admin
+    username: "admin",
+    password: "password", //the proper hash of this password goes in nodered.adminAuth.users[0].password
+    client_id: "node-red-editor"
+  },
+  "nodered": {
+    httpAdminRoot: "/red",
+    httpNodeRoot: "/api",
+    userDir: "./.nodered/",
+    adminAuth: {
+      type: "credentials",
+      users: [{
+        username: "admin",
+        password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
+        permissions: "*"
+      }]
     },
-    "nodered":{ 
-      httpAdminRoot:"/red",
-      httpNodeRoot: "/api",
-      userDir:"./.nodered/",
-      functionGlobalContext: { }    // enables global context
-    }
+    functionGlobalContext: {} // enables global context
+  }
 };
--- index.js
+++ index.js
@@ -2,27 +2,43 @@ var http = require('http');
 var express = require("express");
 var RED = require("node-red");
 var conf = require("./conf/node-red-security-conf");
+var settings = conf.nodered;
+var authMiddleware = require("./lib/auth").bind(this,conf);
+
+
 
 // Create an Express app
 var app = express();
-
+//required by agile not node-red
+app.set('view engine', 'ejs');
 // Add a simple route for static content served from 'public'
 app.use("/",express.static("public"));
 
+//app.use("/red-agile",authMiddleware, express.static("client"));
+app.use("/red-agile",authMiddleware, function (req, res, next){
+    res.render('index', {
+      "user": conf.nodered_integration.username,
+      "password": conf.nodered_integration.password,
+      "client_id": conf.nodered_integration.client_id,
+      "path" : settings.httpAdminRoot
+    });
+});
+
+
 // Create a server
 var server = http.createServer(app);
-
 // Create the settings object - see default settings.js file for other options
-var settings = conf.nodered;
+
+settings.httpAdminAuth = settings.httpAdminAuth || settings.httpAuth;
 
 // Initialise the runtime with a server and settings
 RED.init(server,settings);
 
 // Serve the editor UI from /red
-app.use(settings.httpAdminRoot,RED.httpAdmin);
+app.use(settings.httpAdminRoot, RED.httpAdmin);
 
 // Serve the http nodes UI from /api
-app.use(settings.httpNodeRoot,RED.httpNode);
+app.use(settings.httpNodeRoot, RED.httpNode);
 
 server.listen(conf.security.port);
 
